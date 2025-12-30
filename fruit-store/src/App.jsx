4b756ee7eab0af2c1fcbed02e4c889fa05f03eef
tranmnhat <nhattran281204@gmail.com>
2025-12-30 22:01:07 +0700

    admin & seller

--- fruit-store/src/App.jsx
+++ fruit-store/src/App.jsx
@@ -1,25 +1,16 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import UserLayout from "./app/user/UserLayout";
+import { useRoutes } from "react-router-dom";
+import userRoutes from "./app/user/routes/useroutes";
+import adminRoutes from "./app/admin/routes/adminroutes";
+import sellerRoutes from "./app/seller/routes/sellerroutes";
 
-// USER PAGES
-import Home from "./app/user/pages/home";
-import About from "./app/user/pages/about";
-import Contact from "./app/user/pages/contact";
-import Products from "./app/user/pages/products";
-import ProductDetail from "./app/user/pages/product_detail";
 
 function App() {
-  return (
-    <Routes>
-      <Route element={<UserLayout />}>
-        <Route path="/" element={<Home />} />
-        <Route path="/products" element={<Products />} />
-        <Route path="/products/:id" element={<ProductDetail />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-      </Route>
-    </Routes>
-  );
+  const routes = useRoutes([
+    ...userRoutes,
+    ...adminRoutes,
+    ...sellerRoutes
+  ]);
+  return routes;
 }
 
 export default App;
---
+++ fruit-store/src/app/seller/routes/sellerroutes.jsx
@@ -0,0 +1,10 @@
+import SellerDashBoard from "../pages/sellerdashboard";
+
+const sellerRoutes = [
+  {
+    path: "/seller",
+    element: <SellerDashBoard />,
+  },
+];
+
+export default sellerRoutes;
