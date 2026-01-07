44d55a2e2d4cee7b89fd1487b63517716503815b
ADMIN <lethikimngan.dn43@gmail.com>
2026-01-07 23:03:02 +0700

    Add ProductController and update API routes

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+use App\Http\Controllers\ProductController;
+
+Route::get('/products', [ProductController::class, 'index']);
+
+
+
